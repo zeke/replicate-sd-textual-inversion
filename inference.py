cc225df3d9d58c8b512161a2bbd3fe97e4fd85c1
Zeke Sikelianos <zeke@github.com>
2022-09-26 20:59:36 +0000

    update inference script

--- inference.py
+++ inference.py
@@ -1,28 +1,26 @@
-import argparse
+import os
+from datetime import datetime
 import torch
 from diffusers import StableDiffusionPipeline
 
-
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--model_dir",
-        default="textual_inversion_spyro-dragon",
-    )
-    parser.add_argument(
-        "--prompt",
-        default="Golden Gate Bridge in style of <spyro-dragon>.",
-    )
-    parser.add_argument(
-        "--output_path",
-        default="output.png",
-    )
+    model_dir = "textual_inversion_octocat"
+    prompt = "<octocat> with a cat head and octopus tentacles, dynamic comic hero pose, detailed city at night background, aesthetic, captivating, (((concept art, anime, hyper-detailed and intricate, realistic shaded, fine detail, realistic proportions, symmetrical, sharp focus, 8K resolution, with lineart flat ink, trending on pixiv fanbox)))"
+    pipe = StableDiffusionPipeline.from_pretrained(model_dir,torch_dtype=torch.float16).to("cuda")
 
-    args = parser.parse_args()
-    
-    pipe = StableDiffusionPipeline.from_pretrained(args.model_dir,torch_dtype=torch.float16).to("cuda")
-    with torch.autocast("cuda"):
-        image = pipe(args.prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+    # Create output dir
+    output_dir = f"output/{datetime.today().isoformat()}"
+    os.makedirs(output_dir, exist_ok=True)
 
-    image.save(args.output_path)
+    # Write prompt to text file in output dir
+    with open(os.path.join(output_dir, "prompt.txt"), "w") as f:
+        f.write(prompt) 
+    
+    for i in range(10):
+        output_path = f"{output_dir}/{str(i).zfill(3)}.png"
+        print(output_path)
+
+        with torch.autocast("cuda"):
+            image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+
+        image.save(output_path)
